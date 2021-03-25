da332ba5852e86e8a9dbb54754ac82c963213b59
Jonny Cross <crossjonny5@gmail.com>
2021-03-24 22:16:55 -0700

    initial commit

---
+++ src/com/company/CPU.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class CPU {
+    String byte1;
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	CPU c = new CPU();
+	c.byte1 = args[0];
+	System.out.println(c.byte1);
+    }
+
+}
