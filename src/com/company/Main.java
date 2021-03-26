92fadeaa95fdc6fd7cbaacc3d99e769a13ea8059
Jonny Cross <crossjonny5@gmail.com>
2021-03-25 22:25:29 -0700

    Can now read in file from resources.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,7 @@
 package com.company;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Main {
 
@@ -6,6 +9,20 @@ public class Main {
 	CPU c = new CPU();
 	c.byte1 = args[0];
 	System.out.println(c.byte1);
+
+		try {
+			File myObj = new File("C:\\Users\\cross\\IdeaProjects\\Sim\\resources\\MC.txt");
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				System.out.println(data);
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+
     }
 
 }
