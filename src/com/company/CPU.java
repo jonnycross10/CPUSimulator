1efa50d8fabf1af68ed0977efecddf3ea0ecf5f0
Jonny Cross <crossjonny5@gmail.com>
2021-03-26 00:34:04 -0700

    All in 1 file, and takes filename CL arg

--- src/com/company/CPU.java
+++ src/com/company/CPU.java
@@ -2,13 +2,14 @@ package com.company;
 
 public class CPU {
     String byte1;
-    public void fetch(){
+    public String fetch(){
         //fetches instruction from memory
+        return byte1;
     }
     public void decode(){
         //decodes the fetched instruction
     }
     public void execute(){
-
+    //execute code and increment program counter
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,53 @@
 package com.company;
 
-public class Main {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
+public class Main {
+	public static class CPU {
+		String byte1;
+		public String fetch(){
+			//fetches instruction from memory
+			return byte1;
+		}
+		public void decode(){
+			//decodes the fetched instruction
+		}
+		public void execute(){
+			//execute code and increment program counter
+		}
+	}
+	public static class Memory {
+		int maxMem = 4096; // might be 16 x 4096
+		String[] storage = new String[maxMem];
+		public void initiate(String arg){
+			try {
+				int count = 0;
+				String absPath = "C:\\Users\\cross\\IdeaProjects\\Sim\\resources\\";
+				File myObj = new File(absPath+arg);
+				Scanner myReader = new Scanner(myObj);
+				while (myReader.hasNextLine()) {
+					String data = myReader.nextLine();
+					System.out.println(data);
+					storage[count] = data;
+					count++;
+				}
+				myReader.close();
+			} catch (FileNotFoundException e) {
+				System.out.println("An error occurred.");
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
 	CPU c = new CPU();
 	c.byte1 = args[0];
 	System.out.println(c.byte1);
 	Memory m = new Memory();
-	m.initiate();
+	m.initiate(args[0]);
     }
 
 }
+
