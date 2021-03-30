7160eeb1ae1893dab60b02030176f51daf69576c
Jonny Cross <crossjonny5@gmail.com>
2021-03-29 19:38:21 -0700

    CPU now controls instruction assignment to memory and will strip leading version statement. Stack now holds integers. Memory is 4096 * 16 now.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,35 +6,86 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Main {
 	public static class CPU {
-		String instruction;
+		int instruction;
 		int programCounter=0;
 
-		public void initialize(String arg){
-			// loop through fetch decode and execute
+
+		public void initialize(String[] arg){
+			/*
+
+
+			 */
+			System.out.println(arg.length);
 			Memory m = new Memory();
-			m.initiate(arg);
-			Stack<String> S = new Stack<>();
-			for(int i =0; i<27;i++){
+
+
+
+			int instrLen = initiate(arg[0], m);
+			Stack<Integer> S = new Stack<>();
+			for(int i =0; i<instrLen;i++){
 				instruction = fetch(m,i); // fetch the i'th instruction
 				//System.out.println(instruction);
 
 
-				S = decode(instruction, S);
+				S = decode(instruction, S, m);
 
 			}
-			System.out.println(S.peek());
+			System.out.println(S.size() + "" + S.peek());
 		}
 
-		public String fetch(Memory m, int i){
+		public int initiate(String arg, Memory m){
+			int count = 0;
+			try {
+
+				String absPath = "C:\\Users\\cross\\IdeaProjects\\Sim\\resources\\";
+				File myObj = new File(absPath+arg);
+				Scanner myReader = new Scanner(myObj);
+				while (myReader.hasNextLine()) {
+					String data = myReader.nextLine();
+					System.out.print(count+": ");
+					System.out.println(data);
+					if(count !=0) {
+
+
+						m.storage[count-1] = Integer.parseInt(data, 16);
+					}
+					count++;
+				}
+				myReader.close();
+			} catch (FileNotFoundException e) {
+				System.out.println("An error occurred.");
+				e.printStackTrace();
+			}
+			return(count-1);
+		}
+
+		public String formatHex(String s){//make sure hex string is 4 bits
+			while(s.length()<4){
+				s = "0"+s;
+			}
+			return s;
+		}
+
+		public int fetch(Memory m, int i){
 			//fetches instruction from memory
 
 			return(m.storage[i]);
 		}
-		public Stack<String> decode(String arg, Stack<String> s){
+		public Stack<Integer> decode(int arg, Stack<Integer> s, Memory m){
 			//decodes the fetched instruction
-			if (arg.equals("D000")){ //In CL argument goes to stack
-				s.push("IN");
+			//System.out.println(arg.substring(0,1)); //test
+			String hexString = Integer.toHexString(arg);
+			hexString = formatHex(hexString);
+			System.out.println(hexString);
+			//In port
+			if (hexString.equals("D000")){ //In CL argument goes to stack
+				s.push(0);
 
+			}
+			//Push
+			if(hexString.substring(0,2).equals("10")){ //if the instruction starts with 10
+
+				s.push(m.storage[arg]);
 			}
 
 			return(s);
@@ -44,32 +95,16 @@ public class Main {
 			programCounter++;
 		}
 	}
+
 	public static class Memory {
-		int maxMem = 4096; // might be 16 x 4096
-		String[] storage = new String[maxMem];
-		public void initiate(String arg){
-			try {
-				int count = 0;
-				String absPath = "C:\\Users\\cross\\IdeaProjects\\Sim\\resources\\";
-				File myObj = new File(absPath+arg);
-				Scanner myReader = new Scanner(myObj);
-				while (myReader.hasNextLine()) {
-					String data = myReader.nextLine();
-					//System.out.println(data);
-					storage[count] = data;
-					count++;
-				}
-				myReader.close();
-			} catch (FileNotFoundException e) {
-				System.out.println("An error occurred.");
-				e.printStackTrace();
-			}
-		}
+		int maxMem = 4096 * 16; // might be 16 x 4096
+		Integer[] storage = new Integer[maxMem];
+
 	}
 
 	public static void main(String[] args) {
 	CPU c = new CPU();
-	c.initialize(args[0]);
+	c.initialize(args);
     }
 
 }
