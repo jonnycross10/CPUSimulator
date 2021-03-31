6de832bd334bf5a881f33152dbb713e6536e83f2
Jonny Cross <crossjonny5@gmail.com>
2021-03-31 14:50:39 -0700

    All machine code is decodable, except port instructions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,23 +11,40 @@ public class Main {
 
 
 		public void initialize(String[] arg){
-			/*
 
-
-			 */
-			System.out.println(arg.length);
 			Memory m = new Memory();
+			String option;
+			String fileName = "";
+			int inputInt = 0;
 
+			if(arg.length == 1){
+				fileName = arg[0];
+			}
+			else if(arg.length == 2){
+				if(arg[0].substring(0,1)=="-"){
+					option = arg[0];
+					fileName = arg[1];
+				}
+				else{
+					fileName = arg[0];
+					inputInt = Integer.parseInt(arg[1]);
+				}
+			}
+			else{ //case of 3 arguments
+				option = arg[0];
+				fileName = arg[1];
+				inputInt = Integer.parseInt(arg[2]);
+			}
 
-
-			int instrLen = initiate(arg[0], m);
+			int instrLen = initiate(fileName, m);
 			Stack<Integer> S = new Stack<>();
 			for(int i =0; i<instrLen;i++){
 				instruction = fetch(m,i); // fetch the i'th instruction
 				//System.out.println(instruction);
 
 
-				S = decode(instruction, S, m);
+				S = decode(instruction, S, m, inputInt);
+				programCounter++;
 
 			}
 			System.out.println("Stack size: " + S.size());
@@ -73,17 +90,13 @@ public class Main {
 
 
 		//decode and execute instructions
-		public Stack<Integer> decode(int arg, Stack<Integer> s, Memory m){
+		public Stack<Integer> decode(int arg, Stack<Integer> s, Memory m, int input){
 
 			//System.out.println(arg.substring(0,1)); //test
 			String hexString = Integer.toHexString(arg);
 			hexString = formatHex(hexString);
 			System.out.println(hexString);
-			//In port
-			if (hexString.equals("D000")){ //In CL argument goes to stack
-				s.push(0);
 
-			}
 			//Push I
 			if(hexString.substring(0,2).equals("10")){ //if the instruction starts with 10
 				int i = Integer.parseInt(hexString.substring(2,4),16);
@@ -91,12 +104,212 @@ public class Main {
 				s.push(i);
 			}
 			//Push A
-			if(hexString.substring(0,2).equals("20")){ //if the instruction starts with 10
+			if(hexString.substring(0,2).equals("20")){
 
 				s.push(m.storage[arg]);
 			}
 
-			return(s);
+			//Pop and store in A
+			//implement after IN is finished
+			if(hexString.substring(0,2).equals("30")){
+				//m.storage[arg] = s.pop();
+			}
+
+			//JMP A
+			if(hexString.substring(0,2).equals("40")) {
+				int i = Integer.parseInt(hexString.substring(2,4),16);
+				programCounter = i;
+			}
+
+			//JZ A
+			if(hexString.substring(0,2).equals("50")){
+				if(s.peek()==0) {
+					int i = Integer.parseInt(hexString.substring(2, 4), 16);
+					programCounter = i;
+				}
+			}
+
+			//JNZ A
+			if(hexString.substring(0,2).equals("60")){
+				if(s.peek()!=0) {
+					int i = Integer.parseInt(hexString.substring(2, 4), 16);
+					programCounter = i;
+				}
+			}
+
+			//In port
+			if (hexString.substring(0,2).equals("D0")){ //In CL argument goes to stack
+				s.push(0);
+				//push in[port] to stack
+			}
+
+			//Out Port
+			if (hexString.substring(0,2).equals("E0")){
+				s.push(0);
+				//pop stack onto port
+
+			}
+
+			//Add
+			if (hexString.equals("F000")){
+				int a = s.pop();
+				int b = s.pop();
+				s.push(a+b);
+			}
+
+			//Subtract
+			if (hexString.equals("F001")){
+				int a = s.pop();
+				int b = s.pop();
+				s.push(b-a);
+			}
+
+			//Multiply
+			if (hexString.equals("F002")){
+				int a = s.pop();
+				int b = s.pop();
+				s.push(a*b);
+			}
+
+			//Modulus
+			if (hexString.equals("F003")){
+				int a = s.pop();
+				int b = s.pop();
+				s.push(b%a);
+			}
+
+			//SHL shift left
+			if (hexString.equals("F004")){
+				int a = s.pop();
+				int b = s.pop();
+				int shift = b<<a;
+				s.push(shift);
+			}
+
+			//SHR shift right
+			if (hexString.equals("F005")){
+				int a = s.pop();
+				int b = s.pop();
+				int shift = b>>a;
+				s.push(shift);
+			}
+
+			//BAND B and A
+			if (hexString.equals("F00A")){
+				int a = s.pop();
+				int b = s.pop();
+				int band = b&a;
+				s.push(band);
+			}
+
+			//BOR B or A
+			if (hexString.equals("F00B")){
+				int a = s.pop();
+				int b = s.pop();
+				int shft = b|a;
+				s.push(shft);
+			}
+
+			//BXOR
+			if (hexString.equals("F00C")){
+				int a = s.pop();
+				int b = s.pop();
+				int shft = b^a;
+				s.push(shft);
+			}
+
+			//EQ
+			if (hexString.equals("F010")){
+				int a = s.pop();
+				int b = s.pop();
+				//if equal then push 1 if not then 0
+				if(b == a){
+					s.push(1);
+				}
+				else{
+					s.push(0);
+				}
+			}
+
+			//NE
+			if (hexString.equals("F011")){
+				int a = s.pop();
+				int b = s.pop();
+				//if equal then push 1 if not then 0
+				if(b != a){
+					s.push(1);
+				}
+				else{
+					s.push(0);
+				}
+			}
+
+			//GE
+			if (hexString.equals("F012")){
+				int a = s.pop();
+				int b = s.pop();
+				//if equal then push 1 if not then 0
+				if(b >= a){
+					s.push(1);
+				}
+				else{
+					s.push(0);
+				}
+			}
+
+			//LE
+			if (hexString.equals("F013")){
+				int a = s.pop();
+				int b = s.pop();
+				//if equal then push 1 if not then 0
+				if(b == a){
+					s.push(1);
+				}
+				else{
+					s.push(0);
+				}
+			}
+
+			//GT
+			if (hexString.equals("F014")){
+				int a = s.pop();
+				int b = s.pop();
+				//if equal then push 1 if not then 0
+				if(b > a){
+					s.push(1);
+				}
+				else{
+					s.push(0);
+				}
+			}
+
+			//LT
+			if (hexString.equals("F015")){
+				int a = s.pop();
+				int b = s.pop();
+				//if equal then push 1 if not then 0
+				if(b == a){
+					s.push(1);
+				}
+				else{
+					s.push(0);
+				}
+			}
+
+			//NEG
+			if (hexString.equals("F006")){
+				int a = s.pop();
+				a *= -1;
+				s.push(a);
+			}
+
+			//BNOT Bitwise inversion
+			if (hexString.equals("F00D")){
+				int a = s.pop();
+				s.push(~a);
+			}
+
+				return(s);
 		}
 
 	}
@@ -109,6 +322,7 @@ public class Main {
 
 	public static void main(String[] args) {
 	CPU c = new CPU();
+	System.out.println(args.length);
 	c.initialize(args);
     }
 
